66d3fe8b12d7e599f0dc727249044085fed78aa1
Emrah Bilgic <emrxh@Emrahs-MacBook-Pro.local>
2026-02-06 02:38:25 +0000

    Fix file extension display issue in minfigures

--- script.js
+++ script.js
@@ -16,10 +16,11 @@ const lightboxTitle = document.getElementById('lightbox-title');
 const closeLightbox = document.querySelector('.close-lightbox');
 const lightboxBackdrop = document.querySelector('.lightbox-backdrop');
 
-// Remove extension (case insensitive) and replace special chars
-return filename.replace(/\.[^/.]+$/, "")
-    .replace(/_/g, ' ')
-    .replace(/-/g, ' ');
+function formatTitle(filename) {
+    // Remove extension (case insensitive) and replace special chars
+    return filename.replace(/\.[^/.]+$/, "")
+        .replace(/_/g, ' ')
+        .replace(/-/g, ' ');
 }
 
 // Populate Gallery
